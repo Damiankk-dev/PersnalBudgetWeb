ea39b8215a81a8ddab61c0e14e1994af236175e4
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-08-04 19:41:44 +0200

    final fix, proper popup show, common topnav, expense link, cosmetic styles

--- balancePopup.js
+++ balancePopup.js
@@ -1,23 +1,18 @@
-function showPopUp(){
-	var notH = 1,
-		$pop = $('#popup').hover(function(){notH^=1;});
+function showPopup(e){
+var notH = 1,
+	$pop = $('#popup').hover(function(){notH^=1;});
 
-	$(document).on('mousedown keydown', function( e ){
-	  if(notH||e.which==27) $pop.stop().fadeOut();
-	});
-	$('#cancelBtn').click(function(){
-		 $pop.stop().fadeOut();
-		 });
-	$('#confirmBtn').click(function(){
-		 $pop.stop().fadeOut();
-		 });
+$(document).on('mousedown keydown', function( e ){
+  if(notH||e.which==27) $pop.stop().fadeOut();
+});
+$('#cancelBtn').click(function(){
+	 $pop.stop().fadeOut();
+	 });
+$('#confirmBtn').click(function(){
+	 $pop.stop().fadeOut();
+	 });
 
+$('#popup').css({'top':window.event.pageY-400,'left':window.event.pageX-50, 'position':'absolute'});
+$pop.stop().fadeIn(); 
 
-
-
-
-	/////// CALL POPUP 
-	$('.pop').click(function(){
-	  $pop.stop().fadeIn(); 
-	});
 }
